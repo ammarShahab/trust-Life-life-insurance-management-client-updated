93f5432315080abbd2dbca26a9ee7822e553c26d
Md. Ammar Shahab <ashahab007@gmail.com>
2026-06-29 13:34:29 +0600

    fix: revert axios baseURL to local development environment

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "http://localhost:3000",
-  baseURL: "https://b11a12-server-side-ammar-shahab.vercel.app",
+  baseURL: "http://localhost:3000",
+  // baseURL: "https://b11a12-server-side-ammar-shahab.vercel.app",
 });
 
 const useAxios = () => {
